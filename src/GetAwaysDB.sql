78ede872514ab8a3cb4e81f4c627cd345250410d
IvanAlba97 <ivan02071997@gmail.com>
2023-02-09 18:36:22 +0100

    Corregidas erratas

--- src/GetAwaysDB.sql
+++ src/GetAwaysDB.sql
@@ -10,6 +10,9 @@ CREATE TABLE Ruta_senderismo (
   nombre VARCHAR(255) NOT NULL,
   descripcion TEXT NOT NULL,
   imagen VARCHAR(255) NOT NULL,
+  longitud FLOAT,
+  tipo ENUM ('Lineal', 'Circular', 'Semicircular') NOT NULL,
+  dificultad ENUM ('Fácil', 'Moderado', 'Difícil') NOT NULL,
   permiso_necesario BOOLEAN NOT NULL,
   como_llegar TEXT NOT NULL,
   enlace_maps VARCHAR(255),
@@ -20,16 +23,16 @@ CREATE TABLE Lista_pendientes (
   id INT AUTO_INCREMENT PRIMARY KEY,
   id_usuario INT NOT NULL,
   id_ruta INT NOT NULL,
-  FOREIGN KEY (id_usuario) REFERENCES Usuarios (id),
-  FOREIGN KEY (id_ruta) REFERENCES Rutas_senderismo (id)
+  FOREIGN KEY (id_usuario) REFERENCES Usuario (id),
+  FOREIGN KEY (id_ruta) REFERENCES Ruta_senderismo (id)
 );
 
 CREATE TABLE Lista_realizadas (
   id INT AUTO_INCREMENT PRIMARY KEY,
   id_usuario INT NOT NULL,
   id_ruta INT NOT NULL,
-  FOREIGN KEY (id_usuario) REFERENCES Usuarios (id),
-  FOREIGN KEY (id_ruta) REFERENCES Rutas_senderismo (id)
+  FOREIGN KEY (id_usuario) REFERENCES Usuario (id),
+  FOREIGN KEY (id_ruta) REFERENCES Ruta_senderismo (id)
 );
 
 CREATE TABLE Valoracion (
@@ -39,13 +42,13 @@ CREATE TABLE Valoracion (
   valoracion INT NOT NULL,
   comentario TEXT,
   publica BOOLEAN NOT NULL,
-  FOREIGN KEY (id_usuario) REFERENCES Usuarios (id),
-  FOREIGN KEY (id_ruta) REFERENCES Rutas_senderismo (id)
+  FOREIGN KEY (id_usuario) REFERENCES Usuario (id),
+  FOREIGN KEY (id_ruta) REFERENCES Ruta_senderismo (id)
 );
 
 CREATE TABLE Lista_revisar (
   id INT AUTO_INCREMENT PRIMARY KEY,
   id_usuario INT NOT NULL,
   elemento VARCHAR(255) NOT NULL,
-  FOREIGN KEY (id_usuario) REFERENCES Usuarios (id)
+  FOREIGN KEY (id_usuario) REFERENCES Usuario (id)
 );
