99f7637bb180c2648fed5fcceb17c9cfe763a62a
IvanAlba97 <ivan02071997@gmail.com>
2023-02-16 19:39:30 +0100

    Mejorando la página de detalle de las rutas

--- src/componentes/RouteDetail.js
+++ src/componentes/RouteDetail.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import '../estilos/RouteDetail.css';
 
 function RouteDetail() {
   const { id } = useParams();
@@ -15,14 +16,36 @@ function RouteDetail() {
   }, [id]);
 
   return (
-    <div>
+    <div style={routeDetails ? { 
+      backgroundImage: `url(${routeDetails.imagen})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    } : {}}>
       <Navbar />
-      {routeDetails && (
-        <div>
-          <h3>{routeDetails.nombre}</h3>
-          <p>{routeDetails.descripcion}</p>
-        </div>
-      )}
+      <div className="container">
+        {routeDetails && (
+          <div className="route-details">
+            <h3>{routeDetails.nombre}</h3>
+            <p>{routeDetails.descripcion}</p>
+            <img src={routeDetails.imagen} alt={routeDetails.nombre} />
+            <dl>
+              <dt>Longitud:</dt>
+              <dd>{routeDetails.longitud} km</dd>
+              <dt>Tipo:</dt>
+              <dd>{routeDetails.tipo}</dd>
+              <dt>Permiso necesario:</dt>
+              <dd>{routeDetails.permiso_necesario}</dd>
+              <dt>Cómo llegar:</dt>
+              <dd>{routeDetails.como_llegar}</dd>
+              <dt>Enlace a Google Maps:</dt>
+              <dd><a href={routeDetails.enlace_maps} target="_blank" rel="noopener noreferrer">{routeDetails.enlace_maps}</a></dd>
+              <dt>Media de valoraciones:</dt>
+              <dd>{routeDetails.media_valoraciones}</dd>
+            </dl>
+          </div>
+        )}
+      </div>
       <Footer />
     </div>
   );
