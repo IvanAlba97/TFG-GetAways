1aebf384f23ed738329a57e88c4ea176dcb05725
IvanAlba97 <ivan02071997@gmail.com>
2023-02-23 19:57:59 +0100

    Cambios menores

--- src/componentes/Home.js
+++ src/componentes/Home.js
@@ -6,7 +6,6 @@ import Content from './Content.js';
 import Access from './Access.js';
 
 function Home() {
-  const [loggedIn, setLoggedIn] = useState(false);
   const [user, setUser] = useState(null);
 
   useEffect(() => {
@@ -14,15 +13,13 @@ function Home() {
     fetch('http://localhost:3333/user', { credentials: 'include' })
       .then(response => {
         if (response.ok) {
-          setLoggedIn(true);
           return response.json();
         } else {
-          setLoggedIn(false);
-          throw new Error('No se ha iniciado sesión');
+          //throw new Error('No se ha iniciado sesión');
         }
       })
       .then(data => {
-        setUser(data.user);
+        setUser(data?.user);
       })
       .catch(error => console.error(error));
   }, []);
@@ -33,18 +30,6 @@ function Home() {
         <Route path="/access" element={<Access />} />
       </Routes>
       <Navbar user={user} />
-      {/* {loggedIn ? (
-        <div>
-          {sessionStorage.getItem('session') && JSON.parse(sessionStorage.getItem('session')).user ? (
-            <p>Has iniciado sesión como {JSON.parse(sessionStorage.getItem('session')).user.nombre}</p>
-          ) : (
-            <p>Se ha iniciado sesión pero algo no anda bien</p>
-          )}
-          
-        </div>
-      ) : (
-        <p>No se ha iniciado sesión</p>
-      )} */}
       <Content />
       <Footer />
     </div>
--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import '../estilos/Navbar.css';
 import Logo from '../img/logo.png';
 import Profile from '../img/profile-icon.ico';
@@ -12,11 +11,29 @@ const Navbar = ({ user }) => {
     setMostrarDesplegable(!mostrarDesplegable);
   };
 
+  const handleLogout = () => {
+    fetch('http://localhost:3333/auth/logout', { method: 'POST' })
+      .then(response => {
+        if (response.ok) {
+          // Eliminar información de sesión almacenada en el navegador
+          localStorage.removeItem('token'); // suponiendo que el token de sesión se almacena en localStorage
+          // Redirigir al usuario a la página de inicio
+          window.location.href = '/';
+        } else {
+          // Mostrar un mensaje de error
+        }
+        
+      })
+      .catch(error => {
+        // Mostrar un mensaje de error
+      });
+  };
+
   return (
     <nav>
-      <Link to='/'>
+      <a href='/'>
         <img src={Logo} alt="Logo" />
-      </Link>
+      </a>
       <BarraBusqueda className='barra-busqueda' />
       <div className='contenedor-perfil'>
         {user ? (
@@ -25,19 +42,19 @@ const Navbar = ({ user }) => {
             <img className="contenedor-icono" src={Profile} alt="Profile" />
             {mostrarDesplegable && (
               <div className="desplegable">
-                <Link to='/perfil'>Perfil</Link>
-                <Link to='/equipaje'>Equipaje</Link>
-                <Link to='/rutas-pendientes'>Rutas pendientes</Link>
-                <Link to='/rutas-completadas'>Rutas completadas</Link>
-                <Link to='/cerrar-sesion'>Cerrar sesión</Link>
+                <a href='/perfil'>Perfil</a>
+                <a href='/equipaje'>Equipaje</a>
+                <a href='/rutas-pendientes'>Rutas pendientes</a>
+                <a href='/rutas-completadas'>Rutas completadas</a>
+                <a href='#' onClick={handleLogout}>Cerrar sesión</a>
               </div>
             )}
           </div>
         ) : (
           <div>
-            <Link to="/access">
+            <a href="/access">
               <img className="contenedor-icono" src={Profile} alt="Profile" />
-            </Link>
+            </a>
           </div>
         )
         }
@@ -47,4 +64,3 @@ const Navbar = ({ user }) => {
 };
 
 export default Navbar;
-
