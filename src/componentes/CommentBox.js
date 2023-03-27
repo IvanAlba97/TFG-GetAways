acaf6a67aa1816c975ba7c22a722edbf0997eb5d
IvanAlba97 <ivan02071997@gmail.com>
2023-03-27 11:26:16 +0200

    Mejorada la sección de comentarios cuando no existe ninguno

--- src/componentes/CommentBox.js
+++ src/componentes/CommentBox.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../estilos/CommentBox.css';
 import Profile from '../img/profile-icon.ico';
+import { Link } from 'react-router-dom';
 
 function CommentBox(props) {
   const [comments, setComments] = useState([]);
@@ -27,21 +28,30 @@ function CommentBox(props) {
   }, [props.routeId]);
 
   return (
-    <div style={{width: '100%'}}>
-      {comments.map(comment => (
-        <div className="comment" key={comment.id}>
-          <h3 className="comment-title">{comment.comentario}</h3>
-          <p className="comment-author"><img className="contenedor-icono" src={Profile} alt="Icono-Perfil" />{comment.nombre}</p>
-          <p className="comment-rating-container">
-            {[...Array(comment.valoracion)].map((e, i) => (
-              <span key={i}>★</span>
-            ))}
-          </p>
-          {/* <p className='comment-public'>Pública: {comment.publica === 1 ? 'Sí' : 'No'}</p> */}
-          <p className="comment-date">Fecha: {comment.fecha}</p>
+    <div style={{ width: '100%' }}>
+      {comments.length > 0 ? (
+        comments.map(comment => (
+          <div className="comment" key={comment.id}>
+            <h3 className="comment-title">{comment.comentario}</h3>
+            <p className="comment-author"><img className="contenedor-icono" src={Profile} alt="Icono-Perfil" />{comment.nombre}</p>
+            <p className="comment-rating-container">
+              {[...Array(comment.valoracion)].map((e, i) => (
+                <span key={i}>★</span>
+              ))}
+            </p>
+            <p className="comment-date">Fecha: {comment.fecha}</p>
+          </div>
+        ))
+      ) : (
+        <div className='no-comments'>
+          <p>Nadie ha comentado aún en esta ruta.</p>
+          <Link to="/access" className="comment-button">
+            <button>Inicia sesión para añadir tu comentario.</button>
+          </Link>
         </div>
-      ))}
+      )}
     </div>
+
   );
 }
 
